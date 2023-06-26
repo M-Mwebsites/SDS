343486781d109b43f44a6cfef103061bfb0a7d0b
Miroslav Kopecký <53955446+MiroKopecky@users.noreply.github.com>
2023-06-26 14:28:45 +0200

    try mobile scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,18 +137,6 @@
   }, true)
 
   /**
-   * Scroll with ofset on page load with hash links in the url
-   */
-  window.addEventListener('load', () => {
-    sleep(1000);
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash)
-      }
-    }
-  });
-
-  /**
    * Animation on scroll
    */
   window.addEventListener('load', () => {
@@ -158,6 +146,13 @@
       once: true,
       mirror: false
     })
+    setTimeout(function() {
+      if (window.location.hash) {
+        if (select(window.location.hash)) {
+          scrollto(window.location.hash)
+        }
+      }
+    }, 3000);
   });
 
 
