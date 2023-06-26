6e741c2c28e56899822a8899c83e15f03a0a0583
Miroslav Kopecký <53955446+MiroKopecky@users.noreply.github.com>
2023-06-26 14:24:29 +0200

    logo cursor

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,6 +140,7 @@
    * Scroll with ofset on page load with hash links in the url
    */
   window.addEventListener('load', () => {
+    sleep(1000);
     if (window.location.hash) {
       if (select(window.location.hash)) {
         scrollto(window.location.hash)
